d390e7a394cd18f9528d80a766c0549383c259bf
SergioPerera <alu0101394503@ull.edu.es>
2021-01-04 22:24:52 +0000

    BackUp

--- Dates/src/main_dates.cc
+++ Dates/src/main_dates.cc
@@ -15,9 +15,9 @@ class DateClass {
  
  public:
   DateClass(int, int, int, int);
-  void Print();
-  void NextDates();
-  void LeapYear();
+  void Print(std::ofstream& archive);
+  void NextDates(std::ofstream& archive);
+  void LeapYear(std::ofstream& archive);
 };
 DateClass::DateClass(int day, int month, int year, int later_dates_number){
   day_ = day; 
@@ -25,16 +25,26 @@ DateClass::DateClass(int day, int month, int year, int later_dates_number){
   year_ = year;
   later_dates_number_ = later_dates_number;
 };
-void DateClass::Print(){
-  std::cout << day_ << "/" << month_ << "/" << year_ << "/" << later_dates_number_ <<  std::endl;
+void DateClass::Print(std::ofstream& archive){
+  archive << day_ << "/" << month_ << "/" << year_ << "/" << later_dates_number_ <<  std::endl;
 };
-
-void DateClass::NextDates(){
+void DateClass::NextDates(std::ofstream& archive){
   for(int i{1}; i <= later_dates_number_; i++){
-    
-  }
+    day_  += day_;
 
+    if (day_== 30){
+      month_ += month_;
+      day_ = 1;              //Volvemos al primer día del mes
+
+      if(month_ == 12){
+        year_ += year_;
+        month_ = 1;          //Volvemos al primer mes del año
+      }
+    }
+    archive << day_ << "/" << month_ << "/" << year_ << "/" << std::endl;
+  }
 }
+
 
 
 
@@ -67,15 +77,18 @@ int main (int argc,char *argv[]){
   int month_ {std::stoi(argv[2])};
   int year_ {std::stoi(argv[3])};
   int later_dates_number_ {std::stoi(argv[4])};
-  std::string archive {argv[5]};
+  std::string archive_name {argv[5]};
 
   DateClass in_date(day_, month_, year_, later_dates_number_);
 
   //in_date.Print();
-
-  ofstream archive;
   
-
+  std::ofstream archive;
+  archive.open(archive_name,std::ios::out);
+  if(archive.fail()){
+    std::cout << "Error al crear el archivo" << std::endl;
+    exit(EXIT_SUCCESS);
+  }
   
   
 }

95e5da15b1e2cb2cd119bbd2a34cab0eb7159616
Sergio Perera Márquez <72299705+SergioPerera@users.noreply.github.com>
2021-01-06 01:05:34 +0000

    Add files via upload

---
+++ Dates/CMakeFiles/compiled_dates.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiled_dates.dir/src/dates.cc.o"
+  "CMakeFiles/compiled_dates.dir/src/tools.cc.o"
+  "build/compiled_dates.pdb"
+  "build/compiled_dates"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiled_dates.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

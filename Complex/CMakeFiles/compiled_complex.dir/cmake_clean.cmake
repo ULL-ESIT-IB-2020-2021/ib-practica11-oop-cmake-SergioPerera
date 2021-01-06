0ba8b1bf90679332156cbbdcbbece5462cd135c5
Sergio Perera Márquez <72299705+SergioPerera@users.noreply.github.com>
2021-01-06 01:26:05 +0000

    Add files via upload

---
+++ Complex/CMakeFiles/compiled_complex.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiled_complex.dir/src/main_complex.cc.o"
+  "CMakeFiles/compiled_complex.dir/src/tools.cc.o"
+  "build/compiled_complex.pdb"
+  "build/compiled_complex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiled_complex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

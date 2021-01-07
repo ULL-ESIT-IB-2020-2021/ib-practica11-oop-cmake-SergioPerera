a59a365588ce0c77deb861b104110674ca384725
Sergio Perera Márquez <72299705+SergioPerera@users.noreply.github.com>
2021-01-07 01:29:28 +0000

    Add files via upload

---
+++ Rational/CMakeFiles/compiled_rationals.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiled_rationals.dir/src/main_rationals.cc.o"
+  "CMakeFiles/compiled_rationals.dir/src/tools.cc.o"
+  "build/compiled_rationals.pdb"
+  "build/compiled_rationals"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiled_rationals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Rational/CMakeFiles/tools.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tools.dir/src/tools.cc.o"
+  "lib/libtools.pdb"
+  "lib/libtools.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tools.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
